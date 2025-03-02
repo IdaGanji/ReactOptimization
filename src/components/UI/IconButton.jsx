1d3a37b851342bde97643d1b9ef9dcfe59e39436
Ida Ganji <ida.ganji@omegapoint.no>
2025-03-02 12:50:05 +0100

    Solution: UseCallback hook

--- src/components/UI/IconButton.jsx
+++ src/components/UI/IconButton.jsx
@@ -16,3 +16,10 @@ const IconButtonWrappedWithMemo = memo(
 );
 
 export default IconButtonWrappedWithMemo;
+// even though the component is wrapped with memo, it still gets re-rendered when the 
+// parent component (Counter) gets re-rendered. Why?
+// The reason is that one of the props received from the parent component changes
+// The MinusIcon and PlusIcon are not defined inside the Counter component-instead they are imported from other files
+// The one changing during re-renders is the callback function -> onClick()
+// When the counter component re-renders, the function is created again and is not the same as before in memory
+
