d11d76a5dd0b31d9c3ddaa98a4fbfbb48689046a
Ida Ganji <ida.ganji@omegapoint.no>
2025-03-05 18:41:50 +0100

    learn useMemo hook for optimization

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import {useState, memo, useCallback} from 'react';
+import {useState, memo, useCallback, useMemo} from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
@@ -23,27 +23,29 @@ function isPrime(number) {
             return false;
         }
     }
-
+    console.log("This function executed")
     return true;
 }
+
+// Now, every time our component is re-rendered, This function is called again
+// if there is complex logic and huge code, then it is smart to prevent the function from being called again and again
+// We see that only the initial count can yield to a different value for initialCountIsPrime 
+// Solution: useMemo hook
 
 
 const MemoWrappedCounter = memo(function Counter({initialCount}) {
         log('<Counter /> rendered', 1);
-        const initialCountIsPrime = isPrime(initialCount);
+        // Now this value is re-calculated only if initial count changes and not on every re-render
+        const initialCountIsPrime = useMemo(() => isPrime(initialCount), [initialCount]);
 
         const [counter, setCounter] = useState(initialCount);
-
-        // To avoid re-creation of the handle callback functions in memory, we should wrap them with 
-        // the useCallBack hook. 
-        // state-updating functions are not dependencies
         const handleDecrement = useCallback(function handleDecrement() {
             setCounter((prevCounter) => prevCounter - 1);
         }, [])
         const handleIncrement = useCallback(function handleIncrement() {
             setCounter((prevCounter) => prevCounter + 1);
         }, [])
-    
+
         return (
             <section className="counter">
                 <p className="counter-info">
